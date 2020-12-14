cb962ae28327878ddae7bfc023dc1d34c4d798e8
Bingo <libin4100@gmail.com>
2020-12-14 23:12:23 +0800

    add wp-pass plugin

---
+++ wp-pass/admin.php
@@ -0,0 +1,65 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+final class WP_Pass_Admin {
+    public function __construct() {
+		add_action( 'admin_menu', array( $this, 'toolbar_item' ) );
+    }
+
+	/**
+	 * Toolbar Item
+	 *
+	 * @internal  Private. Called via `wp_before_admin_bar_render` actions.
+	 */
+	public function toolbar_item() {
+		$capability = apply_filters( 'password_protected_options_page_capability', 'manage_options' );
+        add_menu_page(
+            __('Password Protected Pages', 'wp-pass'),
+            __('Password Protected Pages', 'wp-pass'),
+            $capability,
+            'wp-pass-admin',
+            array( $this, 'wp_pass_admin_page' ),
+            'dashicons-lock',
+            22
+        );
+	}
+
+    public function wp_pass_admin_page() {
+        global $wpdb;
+        $query = new WP_Query( array( 'has_password' => true, 'post_type' => 'page', 'post_status' => 'publish' ) );
+        if( $query->have_posts() ) {
+            $results = $wpdb->get_results("select post_id, count(id) as pv, count(distinct email) as uv from " . $wpdb->prefix . "wpp_page_views group by post_id");
+            $views = [];
+            foreach($results as $res) {
+                $views[$res->post_id] = $res;
+            }
+
+            echo '
+<div class="wrap">
+    <div id="icon-options-general" class="icon32"><br /></div>
+    <h2>'.  __( 'Page Views', 'wp-pass' ) . '</h2>
+    <table class="wp-list-table widefat fixed striped table-view-list pages">
+    <thead>
+        <tr>
+            <th scope="col" class="manage-column column-title column-primary sortable desc" id="title"><a href="#"><span>Title</span></a></th>
+            <th scope="col" class="manage-column column-title column-primary sortable desc" id="page-view">Page View</th>
+            <th scope="col" class="manage-column column-title column-primary sortable desc" id="unique-view">Unique View</th>
+        </tr>
+    </thead>
+    <tbody>
+';
+            while( $query->have_posts() ) {
+                $query->the_post();
+                echo '<tr>' .
+                    '<td class="title column-title has-row-actions column-primary page-title"><strong><a class="row-title" href="' . get_the_permalink() . '">' . get_the_title() . '</a></td>' .
+                    '<td>' . (isset($views[get_the_ID()]) ? $views[get_the_ID()]->pv : '') . '</td>' .
+                    '<td>' . (isset($views[get_the_ID()]) ? $views[get_the_ID()]->uv : '') . '</td>' .
+                    '</tr>';
+            }
+            echo '<tbody></table></div>';
+        }
+        wp_reset_postdata();
+    }
+}
+
+$WP_Pass_Admin = new WP_Pass_Admin();
---
+++ wp-pass/wp-pass.php
@@ -0,0 +1,168 @@
+<?php
+/**
+ * Plugin Name: WP Customize Password
+ * Plugin URI: http://www.mywebsite.com/my-first-plugin
+ * Description: WP Customize Password
+ * Version: 1.0
+ * Author: Your Name
+ * Author URI: http://www.mywebsite.com
+ */
+
+if(!defined('ABSPATH')) {
+    exit; // Exit if accessed directly.
+}
+
+
+if(!class_exists('WPPass')):
+/**
+ * Main Class.
+ *
+ */
+final class WPPass {
+    public $version = '1.0.0';
+    public $dbVersion = '1.0.0';
+    public $addonName = 'wp-custom-pass';
+
+    public function __construct() {
+        $this->defines();
+        $this->hooks();
+
+		if ( is_admin() ) {
+			include_once( dirname( __FILE__ ) . '/admin.php' );
+		}
+    }
+
+    public function defines() {
+    }
+
+    public function hooks() {
+        add_filter('the_password_form', [$this, 'customize_the_password_form']);
+        add_filter('post_password_required', [$this, 'customize_post_password_required']);
+        add_action('login_form_custom_pass', [$this, 'customize_pass']);
+
+        register_activation_hook(__FILE__, [$this, 'onActivate']);
+        register_deactivation_hook(__FILE__, [$this, 'onDeactivate']);
+    }
+
+    public function customize_the_password_form() {
+        global $post;
+
+        $labelEmail  = 'pw-email-' . ( empty( $post->ID ) ? rand() : $post->ID );
+        $label  = 'pwbox-' . ( empty( $post->ID ) ? rand() : $post->ID );
+        $output = '<form action="' . esc_url( site_url( 'wp-login.php?action=custom_pass', 'login_post' ) ) . '" class="post-password-form" method="post">
+            <p>' . __( 'This content is password protected. To view it please enter your password below:' ) . '</p>
+            <p><label for="' . $labelEmail . '">' . __( 'Email:' ) . ' <input name="post_email" id="' . $labelEmail . '" type="email" size="20" /></label> </p>
+            <p><label for="' . $label . '">' . __( 'Password:' ) . ' <input name="post_password" id="' . $label . '" type="password" size="20" /></label></p>
+            <p><input type="submit" name="Submit" value="' . esc_attr_x( 'Enter', 'post password form' ) . '" /></p></form>
+';
+        return $output;
+    }
+
+    public function customize_post_password_required() {
+        global $post, $wpdb;
+        if ( empty( $post->post_password ) ) {
+            return false;
+        }
+
+        if ( ! isset( $_COOKIE[ 'wp-postpass_' . COOKIEHASH ] ) ) {
+            return true;
+        }
+
+        require_once ABSPATH . WPINC . '/class-phpass.php';
+        $hasher = new PasswordHash( 8, true );
+
+        $hash = wp_unslash( $_COOKIE[ 'wp-postpass_' . COOKIEHASH ] );
+        if ( 0 !== strpos( $hash, '$P$B' ) ) {
+            $required = true;
+        } else {
+            $required = ! $hasher->CheckPassword( $post->post_password, $hash );
+            if( !$required && $_COOKIE[ 'wp-postemail_' . COOKIEHASH]) {
+                $wpdb->insert($wpdb->prefix . 'wpp_page_views', array (
+                    'post_id' => $post->ID,
+                    'email' => $_COOKIE[ 'wp-postemail_' . COOKIEHASH],
+                    'ip_address' => (!empty($_SERVER['HTTP_CLIENT_IP']) ? $_SERVER['HTTP_CLIENT_IP'] :
+                        (!empty($_SERVER['HTTP_X_FORWARDER_FOR']) ? $_SERVER['HTTP_X_FORWARDER_FOR'] : 
+                            $_SERVER['REMOTE_ADDR']
+                        )
+                    )
+                ), array('%d', '%s', '%s') );
+                unset($_COOKIE[ 'wp-postemail_' . COOKIEHASH]);
+            }
+        }
+
+        return $required;
+    }
+
+    public function customize_pass() {
+		if ( ! array_key_exists( 'post_password', $_POST ) ) {
+			wp_safe_redirect( wp_get_referer() );
+			exit;
+		}
+
+		require_once ABSPATH . WPINC . '/class-phpass.php';
+		$hasher = new PasswordHash( 8, true );
+
+		/**
+		 * Filters the life span of the post password cookie.
+		 *
+		 * By default, the cookie expires 10 days from creation. To turn this
+		 * into a session cookie, return 0.
+		 *
+		 * @since 3.7.0
+		 *
+		 * @param int $expires The expiry time, as passed to setcookie().
+		 */
+		$expire  = apply_filters( 'post_password_expires', time() + 10 * DAY_IN_SECONDS );
+		$referer = wp_get_referer();
+
+		if ( $referer ) {
+			$secure = ( 'https' === parse_url( $referer, PHP_URL_SCHEME ) );
+		} else {
+			$secure = false;
+		}
+
+		setcookie( 'wp-postpass_' . COOKIEHASH, $hasher->HashPassword( wp_unslash( $_POST['post_password'] ) ), $expire, COOKIEPATH, COOKIE_DOMAIN, $secure );
+        if(isset($_POST['post_email']) && $_POST['post_email'])
+            setcookie( 'wp-postemail_' . COOKIEHASH, $_POST['post_email'], $expire, COOKIEPATH, COOKIE_DOMAIN, $secure );
+
+		wp_safe_redirect( wp_get_referer() );
+		exit;
+    }
+
+    public function registerAddon($installedAddons) {
+        $installedAddons[] = ['name' => $this->addonName, 'db_version' => $this->dbVersion, 'version' => $this->version];
+        return $installedAddons;
+    }
+
+    public function onDeactivate() {
+    }
+
+    public function onActivate() {
+
+        global $wpdb;
+
+        $charset_collate = $wpdb->get_charset_collate();
+
+
+        $sqls = [];
+        $sqls[] = "CREATE TABLE " . $wpdb->prefix . "wpp_page_views (
+            id int(11) NOT NULL AUTO_INCREMENT,
+            post_id int(11) NOT NULL,
+            email varchar(128) NOT NULL default '',
+            ip_address varchar(128) NOT NULL default '',
+            view_date datetime NOT NULL default now(),
+            PRIMARY KEY (id)
+        ) CHARSET=utf8";
+
+        if ( ! function_exists( 'dbDelta' ) ) {
+            require( ABSPATH . 'wp-admin/includes/upgrade.php' );
+        }
+
+        foreach($sqls as $sql){
+            error_log(print_r(dbDelta( $sql ), true));
+        }
+    }
+}
+endif;
+
+$WPPass = new WPPass();
