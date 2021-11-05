fba5ab608df810c2a0bf8356fc52825488844c7d
libin4100 <libin4100@gmail.com>
2021-11-06 00:52:08 +0800

    Fix home phone missing issue

--- latepoint-extend/latepoint-extend.php
+++ latepoint-extend/latepoint-extend.php
@@ -360,7 +360,7 @@ EOT;
                 'registered' => $booking->get_meta_by_key('cf_x18jr0Vf', ''),
                 'datetime' => "{$booking->nice_start_time} - {$booking->nice_end_time} ({$booking->nice_start_date})",
                 'phone' => $booking->customer ? $booking->customer->phone : '',
-                'home_phone' => $booking->get_meta_by_key('cf_eh0ZhQ9s', ''),
+                'home_phone' => $booking->customer->get_meta_by_key('cf_eh0ZhQ9s', ''),
                 'type' => $booking->service ? $booking->service->name : '',
                 'reply_by' => $booking->customer ? $booking->customer->get_meta_by_key('cf_nxwjDAcZ', '') : '',
                 'doctor_preference' => $booking->customer ? $booking->customer->get_meta_by_key('cf_7Lkik5fd', '') : '',
