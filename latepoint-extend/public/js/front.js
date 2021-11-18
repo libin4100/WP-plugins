a632a6044951e0ee68b073c0526dd6b3ea65c2df
libin4100 <libin4100@gmail.com>
2021-11-18 18:52:13 +0800

    Button

--- latepoint-extend/public/js/front.js
+++ latepoint-extend/public/js/front.js
@@ -1,5 +1,7 @@
 jQuery(function($) {
     var clinic_btn = '<div class="os-row os-row-btn"><div class="or"><span>or</span></div><div class="os-col-12"><a href="#" class="latepoint-btn latepoint-skip-datetime-btn" data-pre-last-step-label="Submit" data-label="Next Step"><span>I\'m at the clinic</span></a></div></div>';
+    var clinic_notice = '<div class="latepoint-desc-content">If you are booing for a future time: </div>';
+    var clinic_notice2 = '<p>&nbsp;</p><div class="latepoint-desc-content">If you are in clinic already:</div><div class="latepoint-desc-content">Click the button "I\'m at the clinic".</div>';
 
     setInterval(function() {
         if($('.latepoint-body .latepoint-footer.request-move').length) {
@@ -10,6 +12,8 @@ jQuery(function($) {
 
         if((typeof(show_btn) != 'undefined') && $('.step-datepicker-w').length && !$('.step-datepicker-w .os-row-btn').length) {
             $('.step-datepicker-w').append(clinic_btn);
+            $('.latepoint-step-desc .latepoint-desc-media').after(clinic_notice);
+            $('.latepoint-step-desc').append(clinic_notice2);
         }
     }, 100);
 
