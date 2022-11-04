b2b835b1c132b78da1901b629a6dbbf8cfc06986
libin4100 <libin4100@gmail.com>
2022-11-04 17:23:36 +0800

    Simply Benefits

--- latepoint-extend/public/js/front.js
+++ latepoint-extend/public/js/front.js
@@ -60,6 +60,9 @@ jQuery(function($) {
         if($('#booking_custom_fields_cf_qoqkhbly').length && !$('#booking_custom_fields_cf_qoqkhbly').parents('.os-col-12').is(':first-child')) {
             $('#booking_custom_fields_cf_qoqkhbly').parents('.os-col-12').prependTo('.step-custom-fields-for-booking-w.latepoint-step-content .os-row')
         }
+        if($('#booking_custom_fields_cf_vin78day').length && !$('#booking_custom_fields_cf_vin78day').parents('.os-col-12').is(':first-child')) {
+            $('#booking_custom_fields_cf_vin78day').parents('.os-col-12').prependTo('.step-custom-fields-for-booking-w.latepoint-step-content .os-row')
+        }
     }, 100);
 
     $('body').on('click', '.latepoint-body .latepoint-skip-datetime-btn', function() {
@@ -107,6 +110,33 @@ jQuery(function($) {
             data: {
                 action: 'check_certificate',
                 id: $('#booking_custom_fields_cf_qoqkhbly').val()
+            },
+        }).done(function() {
+            $('.latepoint-body #certificate-error').remove();
+        }).always(function() {
+            $('.latepoint-footer .latepoint-next-btn').removeClass('os-loading');
+        }).fail(function(xhr) {
+            if(xhr.status == 404) {
+                if(xhr.responseJSON.data.count >= 3) {
+                    $('.latepoint-footer .latepoint-btn').addClass('disabled');
+                    $('.latepoint-body').empty();
+                }
+
+                if(!$('.latepoint-body #certificate-error').length)
+                    $('.latepoint-body').prepend('<div id="certificate-error" class="latepoint-message latepoint-message-error"></div>');
+                $('.latepoint-body #certificate-error').html(xhr.responseJSON.data.message)
+            }
+        });
+    });
+
+    $('body').on('blur', '#booking_custom_fields_cf_vin78day', function() {
+        $('.latepoint-footer .latepoint-next-btn').addClass('os-loading');
+        $.ajax({
+            method: "POST",
+            url: ajax_object.ajax_url,
+            data: {
+                action: 'check_certificate',
+                id: $('#booking_custom_fields_cf_vin78day').val()
             },
         }).done(function() {
             $('.latepoint-body #certificate-error').remove();
