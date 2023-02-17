767923a4593f38b6abd12b1bf38b3fe87be61aec
libin4100 <libin4100@gmail.com>
2023-02-18 03:09:20 +0800

    Steps

--- latepoint-extend/lib/views/conditions/_step_qhc_service.php
+++ latepoint-extend/lib/views/conditions/_step_qhc_service.php
@@ -30,7 +30,7 @@
             $id = 'qhc_service_' . ($custom_field == 'Other' ? $custom_field : $i);
             echo OsFormHelper::checkbox_field('booking[qhc][' . $custom_field . ']', $custom_field, 'on', ($booking->get_meta_by_key($custom_field, 'off') == 'on'), ['id' => $id], ['class' => 'os-col-12']);
             if ($custom_field == 'Other') {
-                echo OsFormHelper::text_field('booking[qhc][other_detail]', 'Please specify', $booking->get_meta_by_key('other_detail', ''), ['class' => 'os-form-control', 'placeholder' => 'Please specify'], array('class' => 'os-col-12'));
+                echo OsFormHelper::text_field('booking[qhc][other_detail]', 'Please specify', $booking->get_meta_by_key('other_detail', ''), ['class' => 'os-form-control', 'placeholder' => 'Please specify', 'id' => 'other_detail'], array('class' => 'os-col-12'));
             }
         }
         ?>
@@ -39,13 +39,15 @@
 <script>
     jQuery(document).ready(function($) {
         var $other = $('#qhc_service_other');
-        var $otherDetail = $('#qhc_service_other').parent().next();
+        var $otherDetail = $('#other_detail').parents('os-col-12');
         $otherDetail.hide();
         $other.on('change', function() {
             if ($other.is(':checked')) {
                 $otherDetail.show();
+                $otherDetail.find('input').focus().prop('required', true);
             } else {
                 $otherDetail.hide();
+                $otherDetail.find('input').prop('required', false);
             }
         });
     });
