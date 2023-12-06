47913201643d20ec18039d554348b8102a757c75
libin4100 <libin4100@gmail.com>
2023-12-06 23:30:44 +0800

    Update: yourcareaccelerator

--- latepoint-extend/latepoint-extend.php
+++ latepoint-extend/latepoint-extend.php
@@ -377,7 +377,20 @@ jQuery(function($) {
     $('li[data-step-name="custom_fields_for_booking"] span').text('Client Details');
     $('.latepoint-side-panel .latepoint-step-desc .latepoint-desc-title').text('Client Details');
     $('.latepoint-form-w .latepoint-heading-w .os-heading-text').text('Client Details');
-    $('.latepoint-side-panel .latepoint-step-desc-w div[data-step-name="confirmation"] .latepoint-desc-title').text('Request received. We'll contact you soon');
+    $('.latepoint-side-panel .latepoint-step-desc-w div[data-step-name="confirmation"] .latepoint-desc-title').text('Request submitted');
+    $('.latepoint-side-panel .latepoint-step-desc-w div[data-step-name="confirmation"] .latepoint-desc-content').html('Thank you for choosing Gotodoctor.ca. Our team will review your request and contact you within the next 3 business days to collect any additional information required. If you do not hear from us please call us to confirm your request was received.<br><br>* If this is an emergency please go to the nearest hospital or call 911.*');
+});
+</script>
+EOT;
+            }
+            if (OsStepsHelper::$booking_object->agent_id == 12) {
+                echo <<<EOT
+<script>
+jQuery(function($) {
+    $('li[data-step-name="custom_fields_for_booking"] span').text('Client Details');
+    $('.latepoint-side-panel .latepoint-step-desc .latepoint-desc-title').text('Client Details');
+    $('.latepoint-form-w .latepoint-heading-w .os-heading-text').text('Client Details');
+    $('.latepoint-side-panel .latepoint-step-desc-w div[data-step-name="confirmation"] .latepoint-desc-title').text("Request received. We'll contact you soon");
     $('.latepoint-side-panel .latepoint-step-desc-w div[data-step-name="confirmation"] .latepoint-desc-content').html('Thank you for choosing Gotodoctor.ca. <br>Our team will review your request and contact you within the next 3 business days to collect any additional information required. <br>If you do not hear from us please call us to confirm your request was received.<br><br>* If this is an emergency please go to the nearest hospital or call 911.*');
 });
 </script>
