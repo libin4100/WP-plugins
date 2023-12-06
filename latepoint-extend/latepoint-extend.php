4142e8b92a61d737272697f4c831c3e6ca6c8cc8
libin4100 <libin4100@gmail.com>
2023-12-07 01:13:06 +0800

    Fix: DOB on yourcareaccelerator

--- latepoint-extend/latepoint-extend.php
+++ latepoint-extend/latepoint-extend.php
@@ -1551,7 +1551,7 @@ EOT;
         {
             return (
                 (($booking->location_id == 1) && in_array($booking->agent_id, [3, 4]))
-                || in_array($booking->agent_id, [2, 6, 7, 8, 9, 10, 11, 12])
+                || in_array($booking->agent_id, [2, 6, 7, 8, 9, 10, 11])
             ) ? true : false;
         }
 
@@ -2113,7 +2113,7 @@ EOT;
                         ]
                     ],
                     'ic' => [
-                        'show' => ['cf_6A3SfgET', 'cf_sBJs0cqR', 'cf_zZbexFje'],
+                        'show' => ['cf_6A3SfgET', 'cf_sBJs0cqR', 'cf_zZbexFje', 'cf_WFHtiGvf'],
                         'hide' => [
                             'cf_hbCNgimu',
                             'cf_zDS7LUjv',
