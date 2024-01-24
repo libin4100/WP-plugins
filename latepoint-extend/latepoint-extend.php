98974c77d306d5fe9d2377404686b1b09ffcb938
libin4100 <libin4100@gmail.com>
2024-01-24 17:16:03 +0800

    Update version code

--- latepoint-extend/latepoint-extend.php
+++ latepoint-extend/latepoint-extend.php
@@ -23,7 +23,7 @@ if (!class_exists('LatePointExt')) :
      */
     final class LatePointExt
     {
-        public $version = '1.3.14';
+        public $version = '1.3.15';
         public $dbVersion = '1.0.0';
         public $addonName = 'latepoint-extend';
 
