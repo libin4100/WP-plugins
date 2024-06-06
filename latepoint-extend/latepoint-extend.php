50dd0b0bd15f83375bba62115fffa7e34608f15d
libin4100 <libin4100@gmail.com>
2024-06-06 23:15:40 +0800

    SEB

--- latepoint-extend/latepoint-extend.php
+++ latepoint-extend/latepoint-extend.php
@@ -679,7 +679,7 @@ jQuery(function($) {
 </script>
 EOT;
             }
-            if (in_array($bookingObject->agent_id, [11, 15]) && ($bookingObject->location_id != 14)) {
+            if (in_array($bookingObject->agent_id, [11, 15, 16]) && ($bookingObject->location_id != 14)) {
                 $location = $bookingObject->location->name ?? '';
                 echo <<<EOT
 <script>
@@ -746,7 +746,7 @@ EOT;
 <script>
 jQuery(function($) {
     var aid = {$bookingObject->agent_id};
-    var alist = [2, 6, 7, 8, 9, 10, 11, 12, 14, 15];
+    var alist = [2, 6, 7, 8, 9, 10, 11, 12, 14, 15, 16];
     $('body').on('DOMSubtreeModified', '.latepoint-booking-form-element', function() {
         showhide();
     });
@@ -1774,13 +1774,13 @@ EOT;
                 ($booking->service_id == 10)
                 || $this->acorn
                 || $this->covid
-                || ($this->others && (!in_array($booking->agent_id, [6, 8, 11, 15])
+                || ($this->others && (!in_array($booking->agent_id, [6, 8, 11, 15, 16])
                     || (($booking->agent_id == 8) && !in_array($ploc, ['Quebec', 'New Brunswick']))
-                    || (in_array($booking->agent_id, [11, 14, 15]) && !in_array($ploc, ['Quebec']))
+                    || (in_array($booking->agent_id, [11, 14, 15, 16]) && !in_array($ploc, ['Quebec']))
                 ))
                 || $this->diff = (
                     (in_array($booking->agent_id, [2, 7, 9, 10, 14]) && (stripos($loc, $ploc) === false))
-                    || (in_array($booking->agent_id, [11, 15]) && !$this->others && !in_array($ploc, ['Quebec']) && (stripos($loc, $ploc) === false))
+                    || (in_array($booking->agent_id, [11, 15, 16]) && !$this->others && !in_array($ploc, ['Quebec']) && (stripos($loc, $ploc) === false))
                     || (in_array($booking->agent_id, [8]) && !$this->others && (stripos($loc, $ploc) === false))
                 )
             ) {
@@ -1800,7 +1800,7 @@ EOT;
         {
             return (
                 (($booking->location_id == 1) && in_array($booking->agent_id, [3, 4]))
-                || in_array($booking->agent_id, [2, 6, 7, 8, 9, 10, 11, 12, 14, 15])
+                || in_array($booking->agent_id, [2, 6, 7, 8, 9, 10, 11, 12, 14, 15, 16])
             ) ? true : false;
         }
 
