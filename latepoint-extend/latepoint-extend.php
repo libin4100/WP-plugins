43fa7c03c98a07035d39b855bbaeaf2b5916e997
libin4100 <libin4100@gmail.com>
2024-09-12 01:19:15 +0800

    Minor update

--- latepoint-extend/latepoint-extend.php
+++ latepoint-extend/latepoint-extend.php
@@ -639,7 +639,12 @@ jQuery(function($) {
     $('.latepoint-form-w .latepoint-heading-w .os-heading-text').text('Client Details');
     $('.latepoint-side-panel .latepoint-step-desc-w div[data-step-name="confirmation"] .latepoint-desc-title').text('Request submitted');
     $('.latepoint-side-panel .latepoint-step-desc-w div[data-step-name="confirmation"] .latepoint-desc-content').html('{$desc}');
-    $('.latepoint-side-panel .latepoint-step-desc-w div[data-step-name="custom_fields_for_booking"] .latepoint-desc-content').append('<br><br><strong>Submit your request and get a FREE consultation first</strong><br>Payment required only after you would like to proceed');
+    var cfb = $('.latepoint-side-panel .latepoint-step-desc-w .latepoint-step-desc .latepoint-desc-content').text() == $('.latepoint-side-panel .latepoint-step-desc-w div[data-step-name="custom_fields_for_booking"] .latepoint-desc-content').text();
+    var append = '<br><br><strong>Submit your request and get a FREE consultation first</strong><br>Payment required only after you would like to proceed';
+    $('.latepoint-side-panel .latepoint-step-desc-w div[data-step-name="custom_fields_for_booking"] .latepoint-desc-content').append(append);
+    if (cfb) {
+        $('.latepoint-side-panel .latepoint-step-desc-w .latepoint-step-desc .latepoint-desc-content').append(append);
+    }
 });
 </script>
 EOT;
