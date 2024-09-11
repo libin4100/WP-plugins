18e25f397894a1fd443c526e571265cf84a3798d
libin4100 <libin4100@gmail.com>
2024-09-11 14:09:58 +0800

    Leslie group

--- latepoint-extend/latepoint-extend.php
+++ latepoint-extend/latepoint-extend.php
@@ -2003,7 +2003,7 @@ EOT;
         {
             return (
                 (($booking->location_id == 1) && in_array($booking->agent_id, [3, 4]))
-                || in_array($booking->agent_id, [2, 6, 7, 8, 9, 10, 11, 12, 14, 15, 16, 18])
+                || in_array($booking->agent_id, [2, 6, 7, 8, 9, 10, 11, 12, 14, 15, 16, 18, 19, 20])
             ) ? true : false;
         }
 
