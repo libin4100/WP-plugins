2605903cea07adba7b3531ee39020e4c1a215beb
libin4100 <libin4100@gmail.com>
2024-10-07 17:30:40 +0800

    Specialist referral

--- latepoint-extend/lib/views/conditions/_step_qhc_service.php
+++ latepoint-extend/lib/views/conditions/_step_qhc_service.php
@@ -14,7 +14,7 @@
             'Medical marijuana',
             'Pain management',
             'Respiratory care',
-            'Specialist referral',
+            'sr' => 'Specialist referral',
             'mdi' => 'Medical diagnostics and imaging',
             'Paediatric care',
             'Medical procedure/surgery',
@@ -60,20 +60,28 @@
                 "Others - please specify",
             ];
         if ($booking->agent_id == 18) array_unshift($list, 'Medical second opinion');
-        $subMDI = [
-            'MDI: OHIP-covered Service' => 'Looking for an OHIP-covered service',
-            'MDI: Private Service' => 'Looking for the PRIVATE service',
-            'MDI: WISB Coverage' => 'Has WISB coverage'
+        $subs = [
+            'qhc_service_mdi' => [
+                'MDI: OHIP-covered Service' => 'Looking for an OHIP-covered service',
+                'MDI: Private Service' => 'Looking for the PRIVATE service',
+                'MDI: WISB Coverage' => 'Has WISB coverage'
+            ],
+            'qhc_service_sr' => [
+                'SR: OHIP-covered Appointment' => 'Looking for an OHIP-covered appointment',
+                'SR: Private Medical Consultant' => 'Looking for the private medical consultant'
+            ]
         ];
 
         foreach ($list as $i => $custom_field) {
             $id = 'qhc_service_' . (stripos($custom_field, 'other') !== false ? 'other' : $i);
-            echo OsFormHelper::checkbox_field('booking[qhc][services][' . $custom_field . ']', $custom_field, 'on', ($booking->get_meta_by_key($custom_field, 'off') == 'on'), ['id' => $id], ['class' => 'os-col-12']);
+            $options = ['id' => $id];
+            if (is_string($i)) $options['class'] = 'has-sub';
+            echo OsFormHelper::checkbox_field('booking[qhc][services][' . $custom_field . ']', $custom_field, 'on', ($booking->get_meta_by_key($custom_field, 'off') == 'on'), $options, ['class' => 'os-col-12']);
             if ($id == 'qhc_service_other') {
                 echo OsFormHelper::text_field('booking[qhc][services][other_detail]', 'Please specify', $booking->get_meta_by_key('other_detail', ''), ['class' => 'os-form-control', 'placeholder' => 'Please specify', 'id' => 'other_detail'], array('class' => 'os-col-12'));
             }
-            if ($id == 'qhc_service_mdi') {
-                echo OsFormHelper::select_field('booking[qhc][services][mdi_detail]', 'Which service do you need?', $subMDI, $booking->get_meta_by_key('mdi_detail', ''), ['id' => $id . '_detail'], ['class' => 'os-col-12', 'style' => 'display: none;']);
+            if (isset($subs[$id])) {    
+                echo OsFormHelper::select_field("booking[qhc][services][{$i}_detail]", 'Which service do you need?', $subs[$id], $booking->get_meta_by_key('mdi_detail', ''), ['id' => $id . '_detail'], ['class' => 'os-col-12', 'style' => 'display: none;']);
             }
         }
         ?>
@@ -93,11 +101,12 @@
                 }
             });
 
-            $('#qhc_service_mdi').on('change', function() {
+            $('.has-sub').on('change', function() {
+                var detail = $(this).attr('id') + '_detail';
                 if ($(this).is(':checked')) {
-                    $('#qhc_service_mdi_detail').parents('.os-col-12').show();
+                    $('#' + detail).parents('.os-col-12').show();
                 } else {
-                    $('#qhc_service_mdi_detail').parents('.os-col-12').hide();
+                    $('#' + detail).parents('.os-col-12').hide();
                 }
             });
 
