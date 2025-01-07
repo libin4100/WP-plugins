d3dd88a1ccd370f732f838dee983f03d6df40528
libin4100 <libin4100@gmail.com>
2025-01-08 01:53:30 +0800

    Update cleveland

--- latepoint-extend/latepoint-extend.php
+++ latepoint-extend/latepoint-extend.php
@@ -2108,7 +2108,7 @@ EOT;
         {
             return (
                 (($booking->location_id == 1) && in_array($booking->agent_id, [3, 4]))
-                || in_array($booking->agent_id, [2, 6, 7, 8, 9, 10, 11, 12, 14, 15, 16, 18, 19, 20])
+                || in_array($booking->agent_id, [2, 6, 7, 8, 9, 10, 11, 12, 14, 15, 16, 18, 19, 20, 21])
             ) ? true : false;
         }
 
