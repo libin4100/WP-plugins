8321b6bd07f0cd83aa232394b209df25fb2b8be2
libin4100 <libin4100@gmail.com>
2025-04-29 23:40:45 +0800

    Minor update

--- latepoint-extend/lib/views/conditions/_step_qha_time.php
+++ latepoint-extend/lib/views/conditions/_step_qha_time.php
@@ -15,7 +15,7 @@
                 <div class="os-service-selector os-item-i os-animated-self" style="cursor: not-allowed;">
                     <span class="os-item-img-w"><i class="fa fa-calendar-check-o" aria-hidden="true"></i></span>
                     <span class="os-item-name-w">
-                        <span class="os-item-name">Latest available appointment date</span>
+                        <span class="os-item-name">Next available appointment</span>
                         <span class="os-item-desc">We're all set for today. Please go ahead and choose your preferred date and time for the latest next appointment.</span>
                     </span>
                 </div>
