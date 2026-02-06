f4f8cb838423060d93d01d9a326918f3ce0ac2c2
libin4100 <libin4100@gmail.com>
2026-02-06 16:16:06 +0800

    feat(front.js): add French translation for EAP request submission message

--- latepoint-extend/public/js/front.js
+++ latepoint-extend/public/js/front.js
@@ -171,6 +171,9 @@ jQuery(function ($) {
             },
             ".step-custom-fields-for-booking-w": {
                 "ID d'employé MGT": "Numéro d’employé MGT"
+            },
+            ".wpcf7-form .wpcf7-response-output": {
+                "Thank you for submitting your EAP request. We have sent you an email with simple steps to get started on our digital wellness platform.": "Merci d’avoir soumis votre demande au programme d’aide aux employés (PAE). Nous vous avons envoyé un courriel contenant des étapes simples pour commencer à utiliser notre plateforme de bien-être numérique."
             }
         }
         for (let cls in replaces) {
